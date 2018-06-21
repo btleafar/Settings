9b58896829f5dd4188b428c7045fe5bf86d53590
Rafael Torres Balabukha <rafael00.tb@gmail.com>
2018-06-21 13:29:03 -0500

    Adding all files for the 42 header

---
+++ dotfiles/Atom/setup/theme.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# List of atom themes.
+list=(  "city-lights-ui"     \
+		"city-lights-syntax" \
+		"seti-ui"            \
+		"seti-syntax"        )
+
+# Install each theme one by one.
+for thms in ${list[@]}
+do
+		apm install $thms
+done
